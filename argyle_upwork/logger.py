eb63afb84a325a65a2977ea15d79909bd7a11733
Moisés do Nascimento <moises.d.nasci@gmail.com>
2023-11-09 19:51:10 -0300

    :wrench: using rich lib

--- argyle_upwork/logger.py
+++ argyle_upwork/logger.py
@@ -1,10 +1,13 @@
 import logging
 
+from rich.logging import RichHandler
+
+
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    handlers=[logging.StreamHandler()],
+    format="%(message)s",
+    datefmt="[%Y-%m-%d %H:%M:%S]",
+    handlers=[RichHandler(rich_tracebacks=True)],
 )
 
 logger = logging.getLogger(__name__)
