f39eebc56e2883251082a9718375699082a99ff3
Moisés do Nascimento <moises.d.nasci@gmail.com>
2023-11-11 09:46:09 -0300

    :hammer: classes files improved readability

--- argyle_upwork/models/job.py
+++ argyle_upwork/models/job.py
@@ -1,8 +1,50 @@
+"""Model for job objects."""
+
 import re
 from datetime import datetime, timedelta
-from typing import Optional
+from typing import Optional, Union
 
 from pydantic import BaseModel, field_validator
+
+
+def clean_numeric_string(value: Union[str, None]) -> Optional[str]:
+    """Clean a string, extracting numerical values."""
+    if isinstance(value, str):
+        return "".join(filter(str.isdigit, value))
+    return None
+
+
+def validate_client_spendings(value: Union[str, None]) -> Union[str, None]:
+    """Validate the client spendings field."""
+    if isinstance(value, str):
+        value = re.sub(r"[^\d.Kk]", "", value)
+        if "K" in value or "k" in value:
+            value = value.replace("K", "").replace("k", "")
+            return str(float(value) * 1000)
+        return str(value)
+    return value
+
+
+def validate_posted_on(value: Union[str, None]) -> Union[str, None]:
+    """Validate the posted_on field."""
+    if isinstance(value, str):
+        time_units = {
+            "days": "days",
+            "day": "days",
+            "minutes": "minutes",
+            "minute": "minutes",
+            "hours": "hours",
+            "hour": "hours",
+        }
+        match = re.match(r"(\d+) (\w+) ago", value)
+        if match and match.groups()[1] in time_units:
+            unit = time_units[match.groups()[1]]
+            if unit in ("days", "minutes", "hours"):
+                return (
+                    datetime.now()
+                    - timedelta(**{unit: int(match.groups()[0])})
+                ).isoformat()
+    return value
 
 
 class JobSection(BaseModel):
@@ -23,42 +65,16 @@ class JobSection(BaseModel):
     client_spendings: Optional[str] = None
 
     @field_validator("client_spendings")
-    def _validate_client_spendings(cls, v):
+    def validate_client_spendings(cls, v):
         """Validate the client spendings field."""
-        if isinstance(v, str):
-            v = re.sub(r"[^\d.Kk]", "", v)
-            if "K" in v or "k" in v:
-                v = v.replace("K", "").replace("k", "")
-                return str(float(v) * 1000)
-            return str(v)
-        return v
+        return validate_client_spendings(v)
 
     @field_validator("budget")
-    def _extract_numerical_value(cls, v):
-        """Extract the numerical value from the budget field."""
-        if isinstance(v, str):
-            v = "".join(filter(str.isdigit, v))
-            return v
-        return None
+    def extract_numerical_value(cls, v):
+        """Extract the numerical value from the budget string."""
+        return clean_numeric_string(v)
 
     @field_validator("posted_on")
-    def _validate_posted_on(cls, v):
+    def validate_posted_on(cls, v):
         """Validate the posted_on field."""
-        if isinstance(v, str):
-            time_units = {
-                "days": "days",
-                "day": "days",
-                "minutes": "minutes",
-                "minute": "minutes",
-                "hours": "hours",
-                "hour": "hours",
-            }
-            match = re.match(r"(\d+) (\w+) ago", v)
-            if match and match.groups()[1] in time_units:
-                unit = time_units[match.groups()[1]]
-                if unit in ("days", "minutes", "hours"):
-                    return (
-                        datetime.now()
-                        - timedelta(**{unit: int(match.groups()[0])})
-                    ).isoformat()
-        return v
+        return validate_posted_on(v)
--- argyle_upwork/models/profile.py
+++ argyle_upwork/models/profile.py
@@ -1,50 +1,67 @@
+"""Model for profile objects."""
+
 import re
+from typing import List, Union
 
 from pydantic import BaseModel, field_validator
 
 
+def clean_string(
+    value: Union[str, None], *remove_chars: str
+) -> Union[str, None]:
+    """Remove specified characters and strip whitespaces from a string."""
+    if isinstance(value, str):
+        for char in remove_chars:
+            value = value.replace(char, "")
+        return value.strip()
+    return value
+
+
+def strip_whitespace(value: Union[str, None]) -> Union[str, None]:
+    """Strip leading and trailing whitespaces and collapse consecutive spaces."""
+    if isinstance(value, str):
+        return " ".join(value.split())
+    return value
+
+
+def convert_empty_string_to_none(value: str) -> Union[str, None]:
+    """Convert empty string to None."""
+    return None if value == "" else value
+
+
 class ProfilePage(BaseModel):
-    """A Pydantic BaseModel representing a profile section."""
+    """A Pydantic BaseModel representing a profile page."""
 
     title: str
     hourly_rate: str
     description: str
-    skills: list
-    employment_history: list
+    skills: List[str]
+    employment_history: List[dict]
 
     @field_validator("hourly_rate")
     def clean_hourly_rate(cls, value):
         """Extract the numerical value from the hourly rate string."""
-        if isinstance(value, str):
-            return value.replace("$", "").replace("/hr", "").strip()
-        return value
+        return clean_string(value, "$", "/hr")
 
     @field_validator("skills")
     def strip_whitespace_skills(cls, value):
         """Strip leading and trailing whitespaces for each skill in the list."""
-        if isinstance(value, list):
-            return [skill.strip() for skill in value]
-        return value
+        return [strip_whitespace(skill) for skill in value]
 
     @field_validator("employment_history")
     def strip_whitespace_employment_history(cls, value):
         """Strip leading and trailing whitespaces for each employment history entry."""
-        if isinstance(value, list):
-            cleaned_employment_history = []
-            for entry in value:
-                cleaned_entry = {
-                    "title": entry.get("title", "").strip(),
-                    "period": re.sub(
-                        r"\s+", " ", entry.get("period", "")
-                    ).strip(),
-                }
-                cleaned_employment_history.append(cleaned_entry)
-            return cleaned_employment_history
-        return value
+        return [
+            {
+                "title": strip_whitespace(entry.get("title", "")),
+                "period": re.sub(r"\s+", " ", entry.get("period", "")).strip(),
+            }
+            for entry in value
+        ]
 
 
 class AccountSection(BaseModel):
-    """A Pydantic BaseModel representing a account section."""
+    """A Pydantic BaseModel representing an account section."""
 
     user_id: str
     user_name: str
@@ -53,16 +70,14 @@ class AccountSection(BaseModel):
     @field_validator("user_name")
     def strip_whitespace(cls, value):
         """Strip leading and trailing whitespaces and collapse consecutive spaces."""
-        if isinstance(value, str):
-            return " ".join(value.split())
-        return value
+        return strip_whitespace(value)
 
 
 class LocationSection(BaseModel):
-    """A Pydantic BaseModel representing a contact section."""
+    """A Pydantic BaseModel representing a location section."""
 
-    adress_street: str
-    adress_street_2: str
+    address_street: str
+    address_street_2: str
     address_city: str
     address_state: str
     address_zip: str
@@ -72,26 +87,22 @@ class LocationSection(BaseModel):
     @field_validator("phone")
     def strip_whitespace(cls, value):
         """Strip leading and trailing whitespaces and collapse consecutive spaces."""
-        if isinstance(value, str):
-            return "+" + "".join(c for c in value if c.isdigit())
-        return value
+        return "+" + "".join(c for c in value if c.isdigit())
 
     @field_validator("address_zip")
     def convert_empty_string_to_none(cls, value):
         """Convert empty string to None."""
-        if value == "":
-            return None
-        return value
+        return convert_empty_string_to_none(value)
 
     @field_validator("address_state")
     def clean_address_state(cls, value):
         """Remove leading and trailing whitespaces and a comma."""
-        if isinstance(value, str):
-            return value.strip(", ").strip()
-        return value
+        return clean_string(value, ",").strip()
 
 
 class Profile(BaseModel):
+    """A Pydantic BaseModel representing all profile objects scanned."""
+
     account_session: AccountSection
     location_session: LocationSection
     profile_page: ProfilePage
